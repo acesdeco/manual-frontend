93315fa6a40050cc9d0eb9ad9156a2d18c010a98
RightFix <righteousnessude@gmail.com>
2024-10-23 18:34:09 +0100

    dashboard update

--- app/routes/auth.signup.tsx
+++ app/routes/auth.signup.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction, ActionFunctionArgs } from "@remix-run/node";
 import Input from "../components/Input"; // Adjust the import path as necessary
-import { Form } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 import { createUser, IUser } from "~/axios/User";
 import { validateEmail, validateRegNumber } from "~/utils";
 
@@ -155,12 +155,14 @@ export default function Index() {
                 required
               />
             </div>
+            <Link to="../dashboard">
             <button
               type="submit"
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Create Account
             </button>
+            </Link>
           </Form>
         </section>
         <section className="w-1/2 hidden md:flex flex-col justify-center items-center">
