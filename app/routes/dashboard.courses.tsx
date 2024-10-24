e5db2de21f5446e7fe1536e76a8692f60cd89183
Victor Lawrence <viclawrence99@gmail.com>
2024-10-24 17:22:43 +0100

    Touched by divine hands

--- app/routes/dashboard.courses.tsx
+++ app/routes/dashboard.courses.tsx
@@ -2,31 +2,29 @@
 import type { MetaFunction } from "@remix-run/node";
 import { CourseCard } from "~/components/Courses/CourseCard";
 export const meta: MetaFunction = () => {
-    return [
-      { title: "Courses" },
-      { name: "description", content: "View Courses" },
-    ];
-  };
+  return [
+    { title: "Courses" },
+    { name: "description", content: "View Courses" },
+  ];
+};
 const course = {
-    courseCode: "CPE 311",
-    courseLecturer: "Engr. Udofia",
-    courseTime: "2 hours",
-    courseName: "Applications of Programming",
-}
+  courseCode: "CPE 311",
+  courseLecturer: "Engr. Udofia",
+  courseTime: "2 hours",
+  courseName: "Applications of Programming",
+};
 export default function dashboard() {
-    return (
-        <>
-        <header className="mb-10">
-            <h1 className="text-black text-2xl font-semibold">
-                Courses
-            </h1>
-        </header>
-        <section className="grid grid-cols-2 gap-3">
-            <CourseCard course={course}></CourseCard>
-            <CourseCard course={course}></CourseCard>
-            <CourseCard course={course}></CourseCard>
-            <CourseCard course={course}></CourseCard>
-        </section>
-        </>
-    );
+  return (
+    <section className="flex w-full flex-col">
+      <header className="mb-10">
+        <h1 className="text-black text-2xl font-semibold">Courses</h1>
+      </header>
+      <section className="grid grid-cols-2 md:grid-cols-3 gap-3">
+        <CourseCard course={course}></CourseCard>
+        <CourseCard course={course}></CourseCard>
+        <CourseCard course={course}></CourseCard>
+        <CourseCard course={course}></CourseCard>
+      </section>
+    </section>
+  );
 }
