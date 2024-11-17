f02acd980697399425b8a5c175db926625caed11
yusuf-ishaku <kingshakzy@gmail.com>
2024-11-17 20:53:55 +0100

    Arousing cleanliness

--- app/axios/baseConfig.ts
+++ app/axios/baseConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 // const API_URL = process.env.API_URL
 const baseConfig = axios.create({
-  baseURL: `http://localhost:3000/api/v1`, // Replace with your API base URL
+  baseURL: `https://backend-server-6191-2f1fe018-ktb0l1sg.onporter.run/api/v1`, // Replace with your API base URL
   timeout: 10000, // Request timeout in milliseconds
   headers: {
     "Content-Type": "application/json",
