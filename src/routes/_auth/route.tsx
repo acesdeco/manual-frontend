83a077bbc57fe141fd9257ff66011ca5d3d3849e
Yusuf Ishaku <kingshakzy@gmail.com>
2025-09-25 09:05:02 +0100

    Update route.tsx

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -10,7 +10,7 @@ function RouteComponent() {
   return (
     <main className="w-full /h-screen space-y-6 md:px-16 px-4 py-8 fade-in-bottom">
       <header>
-        <h1 className="text-4xl font-bold mb-4">CPE Lab</h1>
+        <h1 className="text-4xl font-bold mb-4">Modools</h1>
         <p className="text-lg text-muted-foreground">
           Modools
         </p>
