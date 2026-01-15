3f393077c44b1760b9d7ac0d93512c72f878b8e3
Winner Edwin <winneredwin2667@gmail.com>
2026-01-15 22:27:04 +0100

    update base url for api in prod

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,8 +1,10 @@
 import ky, { type KyResponse } from "ky";
 import z from "zod";
 
-// const BASE_API_URL = "https://manual-backend-production-b53b.up.railway.app";
-const BASE_API_URL = "http://localhost:3000";
+const BASE_API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://manual-backend-production-b53b.up.railway.app";
 
 export const api = ky.create({
   prefixUrl: new URL("/api/v1", BASE_API_URL),
